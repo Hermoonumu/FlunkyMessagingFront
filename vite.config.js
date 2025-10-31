4a3ff032f8c138c96d4d30c42ad4c2133dec1a25
Hermoonumu <geron9988@gmail.com>
2025-10-31 08:43:18 +0100

    vite conf

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   server: {
     allowedHosts: ['flunkymessagingfront.onrender.com'],
-    allowedHosts: ['flunkymessagingapp.onrender.com']
     // If you want to allow all hosts (less secure), you can set:
     // allowedHosts: true
   },
